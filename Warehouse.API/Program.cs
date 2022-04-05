7caa7cd2fb96153485baba74b8ce9fcf2e74dbca
Arseni Klimenka <arseni.klimenka.job@gmail.com>
2022-04-05 20:59:32 +0300

    Setting up mongoDB (work in progress)

--- Warehouse.API/Program.cs
+++ Warehouse.API/Program.cs
@@ -1,3 +1,6 @@
+using Warehouse.Application;
+using Warehouse.Domain.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
+builder.Services.Configure<MongoDbConfiguration>(builder.Configuration.GetSection("MongoConnectionSettings"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Warehouse.Core/MongoBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Domain.Helpers;
+
+namespace Warehouse.Infrastructure
+{
+    public class MongoBaseContext<T>
+    {
+        private readonly IMongoCollection<T> collection;
+        private readonly MongoDbConfiguration _settings;
+        public MongoBaseContext(IOptions<MongoDbConfiguration> settings)
+        {
+            _settings = settings.Value;
+            var client = new MongoClient(_settings.ConnectionString);
+            var database = client.GetDatabase(_settings.DatabaseName);
+            collection = database.GetCollection<T>(nameof(T));
+        }
+    }
+}
